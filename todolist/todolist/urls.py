a8c6c65db870e4b97c604e3f25cf2abad45f9e09
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-08 18:46:08 +0300

    Update urls.py

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,9 +1,4 @@
-"""todolist URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-
-"""
 from django.contrib import admin
 from django.urls import path,include
 
