4a7a33fa9eaa9ab15ca2056f75a14f3b951af293
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-08 18:42:29 +0300

    Update urls.py

--- englishdictionary/englishdictionary/urls.py
+++ englishdictionary/englishdictionary/urls.py
@@ -1,14 +1,3 @@
-"""
-URL configuration for englishdictionary project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
 
 from django.contrib import admin
 from django.urls import path,include
