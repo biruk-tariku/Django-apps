72d0646ceb47cbe7c31a2a34b8bd266c05bf4045
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-08 18:45:40 +0300

    Update urls.py

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -5,9 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    a URL to urlpatterns:  path('blog/', include('blog.urls'))
+   
 """
 from django.contrib import admin
 from django.urls import path,include
