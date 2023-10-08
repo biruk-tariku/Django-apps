b253a22bfa13226a27027f774b9653c5a0d436d8
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-08 18:43:46 +0300

    Update urls.py

--- pyshop/pyshop/urls.py
+++ pyshop/pyshop/urls.py
@@ -1,8 +1,3 @@
-
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-
 from django.contrib import admin
 from django.urls import path,include
 
