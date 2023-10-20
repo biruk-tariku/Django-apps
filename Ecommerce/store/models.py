3f399e0c3f86eda4df8484a08f4dfccf72984c2b
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-20 13:30:21 +0300

    Update models.py

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 import datetime
 
+
 class category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name   
-
 
     
     class Meta:
