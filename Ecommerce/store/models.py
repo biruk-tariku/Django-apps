d88ce74dac831aee45138d5a29b2667949c545ad
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-10-20 13:23:55 +0300

    Add files via upload

---
+++ Ecommerce/store/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+import datetime
+
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name   
+
+    
+    class Meta:
+        verbose_name_plural = 'categories' 
+
+
+class customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+
+
+class product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+
+
+    def __str__(self):
+        return self.name 
+
+class order(models.Model):
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='',blank=True)
+    phone = models.CharField(max_length=20, default='',blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.product
+
+
+
